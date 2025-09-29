54fafb71603bfa8874e711e15ae68569f2ff9a1e
barust-a <alexandrebarusta@gmail.com>
2025-09-29 23:19:45 +0200

    enable cors + redirection

--- src/api/url-shortener/url-shortener.controller.ts
+++ src/api/url-shortener/url-shortener.controller.ts
@@ -1,21 +1,26 @@
-import { Controller, Post, Get, Param, Body } from "@nestjs/common"
+import { Controller, Post, Get, Param, Body, Res } from "@nestjs/common"
 import { UrlShortenerService } from "./url-shortener.service"
+import type { Response } from "express"
 
 @Controller()
 export class UrlShortenerController {
   constructor(private readonly urlService: UrlShortenerService) {}
 
   @Post()
-  async createShortUrl(@Body("url") url: string) {
+  async createShortUrl(@Body("url") url: string): Promise<string> {
     const existingUrl = await this.urlService.getShortUrlFromLongUrl(url)
-    return existingUrl || (await this.urlService.createShortUrl(url))
+    const shortUrl = existingUrl || (await this.urlService.createShortUrl(url))
+    return shortUrl
   }
 
   @Get(":id")
-  async getLongUrlFromId(@Param("id") id: string) {
+  async getLongUrlFromId(
+    @Param("id") id: string,
+    @Res() res: Response,
+  ): Promise<void> {
     const url = await this.urlService.getLongUrlFromId(id)
 
     await this.urlService.incrementClicks(id)
-    return url
+    return res.redirect(url)
   }
 }
