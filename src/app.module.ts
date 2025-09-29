ae8ab08d9c311b0aa367874aaf7259a4478c6b94
barust-a <alexandrebarusta@gmail.com>
2025-09-29 21:38:39 +0200

    create base modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { UrlShortenerModule } from './api/url-shortener/url-shortener.module';
+import { CleanupModule } from './cron/cleanup/cleanup.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UrlShortenerModule, CleanupModule],
   controllers: [AppController],
   providers: [AppService],
 })
